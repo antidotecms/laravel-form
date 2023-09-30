c7070f9c2d66c900cf31cadf370453790642f493
Tim Smith <tim.smith@titan21.co.uk>
2023-09-30 17:05:17 +0100

    Add soft deletes to Field model

--- packages/form/src/Models/Field.php
+++ packages/form/src/Models/Field.php
@@ -8,6 +8,7 @@ use Illuminate\Contracts\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Support\Str;
 use Spatie\EloquentSortable\Sortable;
 use Spatie\EloquentSortable\SortableTrait;
@@ -16,6 +17,7 @@ class Field extends Model implements Sortable
 {
     use HasFactory;
     use SortableTrait;
+    use SoftDeletes;
 
     public static $modelsShouldPreventAccessingMissingAttributes = true;
 
